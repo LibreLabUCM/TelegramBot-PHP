d690552ec41170b30cec92571c8074f05d9b665b
DevPGSV <DevPGSV@gmail.com>
2016-02-07 18:03:43 +0100

    Added TA_File (Telegram Api File Object)

--- TelegramBot/TelegramApi.php
+++ TelegramBot/TelegramApi.php
@@ -511,3 +511,41 @@ class TA_Message {
 	// TODO Add all getters
 
 }
+
+class TA_File {
+	private $file_id;
+	private $file_size;
+	private $file_path;
+	
+	public function TA_File($file_id, $file_size = null, $file_path = null) {
+		$this->file_id = $file_id;
+		$this->file_size = $file_size;
+		$this->file_path = $file_path;
+	}
+	
+	public static function createFromJson($json) {
+		return TA_File::createFromArray(json_decode($json));
+	}
+	
+	public static function createFromArray($arr) {
+		return new Self(
+				$arr['file_id'],
+				isset($arr['file_size'])	? $arr['file_size']	: null,
+				isset($arr['$file_path'])	? $arr['$file_path']: null
+			);
+	}
+	
+	public function getFileId() {
+		return $this->file_id;
+	}
+	
+	public function getFileSize() {
+		return $this->file_size;
+	}
+	
+	public function getFilePath() {
+		return $this->file_path;
+	}
+}
+
+
