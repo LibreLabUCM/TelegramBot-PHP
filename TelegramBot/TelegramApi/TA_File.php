931514704df18e93960c59a63aa8e927b93b829c
DevPGSV <DevPGSV@gmail.com>
2016-03-19 19:50:06 +0100

    Added TA_Document

--- TelegramBot/TelegramApi/TA_File.php
+++ TelegramBot/TelegramApi/TA_File.php
@@ -89,5 +89,98 @@ class TA_File {
    */
   public function downloadFile() {
     // https://api.telegram.org/file/bot<token>/<file_path>
+    return $this->_api->downloadFile($this->getFilePath());
   }
+}
+
+class TA_Document {
+  private $_api; // TelegramApi
+  private $file_id;
+  private $thumb;
+  private $file_name;
+  private $mime_type;
+  private $file_size;
+  private $_file;
+
+  private function TA_Document(TelegramApi $api, $file_id, $thumb, $file_name, $mime_type, $file_size) {
+    $this->_api = $api;
+    $this->file_id = $file_id;
+    $this->thumb = $thumb;
+    $this->file_name = $file_name;
+    $this->mime_type = $mime_type;
+    $this->file_size = $file_size;
+  }
+
+  /**
+   * Creates a TA_Document from a json string
+   *
+   * @param string $api
+   *        	an instance to the TelegramApi wrapper
+   * @param array $json
+   *        	a json string representing a TA_Document
+   *
+   * @return a TA_Document object
+   */
+  public static function createFromJson(TelegramApi $api, $json) {
+    return TA_Document::createFromArray($api, json_decode($json));
+  }
+
+  /**
+   * Creates a TA_Document from an associative array
+   *
+   * @param string $api
+   *        	an instance to the TelegramApi wrapper
+   * @param array $json
+   *        	an associative array representing a TA_Document
+   *
+   * @return a TA_Document object
+   */
+  public static function createFromArray(TelegramApi $api, $arr) {
+    return new Self(
+          $api,
+          $arr['file_id'],
+          isset($arr['thumb'])       ? TA_File::createFromArray($api, $arr['thumb']) : null,
+          isset($arr['file_name'])   ? $arr['file_name']  : null,
+          isset($arr['mime_type'])   ? $arr['mime_type']  : null,
+          isset($arr['file_size'])   ? $arr['file_size']  : null
+        );
+  }
+
+  /**
+   * Gets the file id
+   *
+   * @return string file id
+   */
+  public function getFileId() {
+    return $this->file_id;
+  }
+
+  /**
+   * Gets the file size
+   *
+   * @return int file size
+   */
+  public function getFileSize() {
+    return $this->file_size;
+  }
+
+  /**
+   * Gets the file name
+   *
+   * @return string file name
+   */
+  public function getFileName() {
+    return $this->file_name;
+  }
+
+  private function updateFile() {
+    $this->_file = TA_File::createFromArray($this->_api, $this->_api->getFile($this->getFileId()));
+  }
+
+  public function downloadFile() {
+    if ($this->_file == null)
+      $this->updateFile();
+    return $this->_file->downloadFile();
+  }
+
 }
