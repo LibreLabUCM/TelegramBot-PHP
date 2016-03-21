13badab0d7453e890accb70ed3614d0113ca270c
DevPGSV <DevPGSV@gmail.com>
2016-03-21 17:17:26 +0100

    Implemented sendVoice function in TelegramApi and fixed typo

--- TelegramBot/TelegramApi/TelegramApi.php
+++ TelegramBot/TelegramApi/TelegramApi.php
@@ -226,10 +226,28 @@ class TelegramApi {
   }
 
   // Untested
-  public function sendVoice($chat_id, $vioce, $duration, $reply_id = null, $reply_markup = null) {
+  public function sendVoice($chat_id, $voice, $duration, $disable_notification = false, $reply_id = null, $reply_markup = null) {
     if ($chat_id instanceof TA_User) $chat_id = $chat_id->getId();
 
-    // TODO
+    $options = array ();
+    $options ['chat_id'] = $chat_id;
+    $options ['voice'] = $voice;
+
+    if ($duration !== null) {
+      $options ['duration'] = $duration;
+    }
+
+    $options ['disable_notification'] = $disable_notification;
+
+    if ($reply_id !== null) {
+      $options ['reply_id'] = $reply_id;
+    }
+
+    if ($reply_markup !== null) {
+      $options ['reply_markup'] = json_encode ( $reply_markup );
+    }
+
+    return TA_Message::createFromArray($this, $this->sendApiRequest ( 'sendVoice', $options ));
   }
 
   /**
