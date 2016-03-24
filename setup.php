0690420d09ccf47457c71b290fdf5f69e7f8305b
DevPGSV <DevPGSV@gmail.com>
2016-03-24 22:16:00 +0100

    Check if config file exists. If not, redirect to install.php

--- setup.php
+++ setup.php
@@ -1,9 +1,21 @@
 <?php
 ini_set('display_errors','1'); error_reporting(E_ALL);
+if (!file_exists('./config_file.php')) {
+  header('Location: install.php');
+  echo 'install first!';
+  exit();
+} else {
+    require_once('./config_file.php'); // $_BOT_CONFIG
+    if (!isset($_BOT_CONFIG)) {
+      header('Location: install.php');
+      echo 'install first!';
+      exit();
+    }
+}
 
 require_once('./TelegramBot/TelegramBot.php');
 require_once('./TelegramBot/BotConfig.php');
-require_once('./config_file.php'); // $_BOT_CONFIG
+
 
 $config = BotConfig::create()
   -> setToken($_BOT_CONFIG['token'])
