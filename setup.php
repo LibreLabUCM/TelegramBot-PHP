bf7f6bb46f93f48c5d2c02041b641bce4ee407e1
DevPGSV <DevPGSV@gmail.com>
2016-03-24 19:16:39 +0100

    Included new property into configuration

--- setup.php
+++ setup.php
@@ -8,15 +8,15 @@ require_once('./config_file.php'); // $_BOT_CONFIG
 $config = BotConfig::create()
   -> setToken($_BOT_CONFIG['token'])
   -> setWebhookUrl($_BOT_CONFIG['baseUrl'].'bot.php')
+  -> setBaseUrl($_BOT_CONFIG['baseUrl'])
   -> setDb($_BOT_CONFIG['db'])
   -> setAdmins($_BOT_CONFIG['admins'])
   -> setHookKey($_BOT_CONFIG['hookKey'])
   -> validate();
+unset($_BOT_CONFIG);
 
 try {
   $bot = new TelegramBot($config);
-  unset($_BOT_CONFIG);
-  unset($config);
   return $bot;
 } catch (InvalidConfigException $e) {
   http_response_code(500);
