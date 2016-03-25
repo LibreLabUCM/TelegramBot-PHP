a5c4e69367a368964feb2c8de275359e3c5b8640
DevPGSV <DevPGSV@gmail.com>
2016-03-25 19:11:59 +0100

    Implemented TA_InlineQueryResultPhoto, TA_InlineQueryResultGif y TA_InlineQueryResultMpeg4Gif

--- src/TelegramBot/TelegramApi/TA_InlineQueryResult.php
+++ src/TelegramBot/TelegramApi/TA_InlineQueryResult.php
@@ -152,10 +152,16 @@ class TA_InlineQueryResultArticle extends TA_InlineQueryResult {
 class TA_InlineQueryResultPhoto extends TA_InlineQueryResult {
   private $photo_url;
   private $thumb_url;
-
   private $title;
+  private $photo_width;
+  private $photo_height;
+  private $description;
+  private $caption;
+  private $message_text;
+  private $parse_mode;
+  private $disable_web_page_preview;
 
-  public function TA_InlineQueryResultPhoto($api, $id, $photo_url, $thumb_url = null, $title = null) {
+  public function TA_InlineQueryResultPhoto($api, $id, $photo_url, $thumb_url = null, $title = null, $photo_width = null, $photo_height = null, $description = null, $caption = null, $message_text = null, $parse_mode = null, $disable_web_page_preview = null) {
     if ($thumb_url === null) $thumb_url = $photo_url;
 
     $this->_api = $api;
@@ -164,8 +170,14 @@ class TA_InlineQueryResultPhoto extends TA_InlineQueryResult {
     $this->photo_url = $photo_url;
     $this->thumb_url = $thumb_url;
     $this->title = $title;
+    $this->photo_width = $photo_width;
+    $this->photo_height = $photo_height;
+    $this->description = $description;
+    $this->caption = $caption;
+    $this->message_text = $message_text;
+    $this->parse_mode = $parse_mode;
+    $this->disable_web_page_preview = $disable_web_page_preview;
   }
-
 
   public function toArr(){
     $ret = array();
@@ -175,6 +187,13 @@ class TA_InlineQueryResultPhoto extends TA_InlineQueryResult {
     $ret['thumb_url']      = $this->thumb_url;
 
     if (isset($this->title))             $ret['title']              = $this->title;
+    if (isset($this->photo_width))       $ret['photo_width']        = $this->photo_width;
+    if (isset($this->photo_height))      $ret['photo_height']       = $this->photo_height;
+    if (isset($this->description))       $ret['description']        = $this->description;
+    if (isset($this->caption))           $ret['caption']            = $this->caption;
+    if (isset($this->message_text))      $ret['message_text']       = $this->message_text;
+    if (isset($this->parse_mode))        $ret['parse_mode']         = $this->parse_mode;
+    if (isset($this->disable_web_page_preview))   $ret['disable_web_page_preview']   = $this->disable_web_page_preview;
     return $ret;
   }
 }
@@ -186,7 +205,52 @@ class TA_InlineQueryResultPhoto extends TA_InlineQueryResult {
  *
  */
 class TA_InlineQueryResultGif extends TA_InlineQueryResult {
-  public function toArr(){}
+  private $gif_url;
+  private $thumb_url;
+  private $title;
+  private $gif_width;
+  private $gif_height;
+  private $description;
+  private $caption;
+  private $message_text;
+  private $parse_mode;
+  private $disable_web_page_preview;
+
+  public function TA_InlineQueryResultGif($api, $id, $gif_url, $thumb_url = null, $title = null, $gif_width = null, $gif_height = null, $description = null, $caption = null, $message_text = null, $parse_mode = null, $disable_web_page_preview = null) {
+    if ($thumb_url === null) $thumb_url = $gif_url;
+
+    $this->_api = $api;
+    $this->type = "gif";
+    $this->id = (string)$id;
+    $this->gif_url = $gif_url;
+    $this->thumb_url = $thumb_url;
+    $this->title = $title;
+    $this->gif_width = $gif_width;
+    $this->gif_height = $gif_height;
+    $this->description = $description;
+    $this->caption = $caption;
+    $this->message_text = $message_text;
+    $this->parse_mode = $parse_mode;
+    $this->disable_web_page_preview = $disable_web_page_preview;
+  }
+
+  public function toArr(){
+    $ret = array();
+    $ret['type']           = $this->type;
+    $ret['id']             = $this->id;
+    $ret['gif_url']        = $this->gif_url;
+    $ret['thumb_url']      = $this->thumb_url;
+
+    if (isset($this->title))             $ret['title']              = $this->title;
+    if (isset($this->gif_width))         $ret['gif_width']          = $this->gif_width;
+    if (isset($this->gif_height))        $ret['gif_height']         = $this->gif_height;
+    if (isset($this->description))       $ret['description']        = $this->description;
+    if (isset($this->caption))           $ret['caption']            = $this->caption;
+    if (isset($this->message_text))      $ret['message_text']       = $this->message_text;
+    if (isset($this->parse_mode))        $ret['parse_mode']         = $this->parse_mode;
+    if (isset($this->disable_web_page_preview))   $ret['disable_web_page_preview']   = $this->disable_web_page_preview;
+    return $ret;
+  }
 }
 
 /**
@@ -196,7 +260,52 @@ class TA_InlineQueryResultGif extends TA_InlineQueryResult {
  *
  */
 class TA_InlineQueryResultMpeg4Gif extends TA_InlineQueryResult {
-  public function toArr(){}
+  private $mpeg4_url;
+  private $thumb_url;
+  private $title;
+  private $mpeg4_width;
+  private $mpeg4_height;
+  private $description;
+  private $caption;
+  private $message_text;
+  private $parse_mode;
+  private $disable_web_page_preview;
+
+  public function TA_InlineQueryResultMpeg4Gif($api, $id, $mpeg4_url, $thumb_url = null, $title = null, $mpeg4_width = null, $mpeg4_height = null, $description = null, $caption = null, $message_text = null, $parse_mode = null, $disable_web_page_preview = null) {
+    if ($thumb_url === null) $thumb_url = $mpeg4_url;
+
+    $this->_api = $api;
+    $this->type = "mpeg4_gif";
+    $this->id = (string)$id;
+    $this->mpeg4_url = $mpeg4_url;
+    $this->thumb_url = $thumb_url;
+    $this->title = $title;
+    $this->mpeg4_width = $mpeg4_width;
+    $this->mpeg4_height = $mpeg4_height;
+    $this->description = $description;
+    $this->caption = $caption;
+    $this->message_text = $message_text;
+    $this->parse_mode = $parse_mode;
+    $this->disable_web_page_preview = $disable_web_page_preview;
+  }
+
+  public function toArr(){
+    $ret = array();
+    $ret['type']           = $this->type;
+    $ret['id']             = $this->id;
+    $ret['mpeg4_url']        = $this->mpeg4_url;
+    $ret['thumb_url']      = $this->thumb_url;
+
+    if (isset($this->title))             $ret['title']              = $this->title;
+    if (isset($this->mpeg4_width))       $ret['mpeg4_width']        = $this->mpeg4_width;
+    if (isset($this->mpeg4_height))      $ret['mpeg4_height']       = $this->mpeg4_height;
+    if (isset($this->description))       $ret['description']        = $this->description;
+    if (isset($this->caption))           $ret['caption']            = $this->caption;
+    if (isset($this->message_text))      $ret['message_text']       = $this->message_text;
+    if (isset($this->parse_mode))        $ret['parse_mode']         = $this->parse_mode;
+    if (isset($this->disable_web_page_preview))   $ret['disable_web_page_preview']   = $this->disable_web_page_preview;
+    return $ret;
+  }
 }
 
 /**
