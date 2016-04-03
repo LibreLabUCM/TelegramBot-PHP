954f7a1243ae1113e30fa2a72b05c2bf156f0393
DevPGSV <DevPGSV@gmail.com>
2016-04-03 18:46:15 +0200

    Updated helpPlugin

--- src/TelegramBot/plugins/helpPlugin.php
+++ src/TelegramBot/plugins/helpPlugin.php
@@ -1,17 +1,21 @@
 <?php
 require_once(__DIR__ . '/../PluginManager.php');
 
-return 'HelpPlugin';
+
 class HelpPlugin extends TB_Plugin {
   public function HelpPlugin($api) {
     parent::__construct($api);
   }
 
+  /**
+   * %condition text matches ^\/(?:help|start)(?:@{#USERNME})?$
+   */
   public function onMessageReceived($message) {
-    if ($message->hasText()) {
-      if ($message->getText() === "/help" || $message->getText() === "/start") {
-        $this->api->sendMessage($message->getFrom(), "Developing... If you want to /test ...");
-      }
-    }
+    $message->sendReply("Developing... If you want to /test ...");
   }
 }
+return array(
+  'class' => 'HelpPlugin',
+  'name' => 'Help',
+  'id' => 'HelpPlugin'
+);
