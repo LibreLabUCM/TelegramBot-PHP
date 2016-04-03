7c7c2b1057210865613b9b8b5a6cd7b971cb5a8e
DevPGSV <DevPGSV@gmail.com>
2016-04-03 20:47:32 +0200

    Fixed typos

--- src/TelegramBot/plugins/examplePlugin.php
+++ src/TelegramBot/plugins/examplePlugin.php
@@ -3,7 +3,7 @@ require_once(__DIR__ . '/../PluginManager.php');
 
 
 class ExamplePlugin extends TB_Plugin {
-  public function HelloWorldPlugin($api) {
+  public function ExamplePlugin($api) {
     parent::__construct($api);
   }
 
@@ -35,6 +35,6 @@ class ExamplePlugin extends TB_Plugin {
 }
 return array(
   'class' => 'ExamplePlugin',
-  'name' => 'Example World',
+  'name' => 'Example Plugin',
   'id' => 'ExamplePlugin'
 );
