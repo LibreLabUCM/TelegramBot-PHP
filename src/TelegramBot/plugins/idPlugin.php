cef098a1ec2256b860a4c414b648b69f23aaf6c1
DevPGSV <DevPGSV@gmail.com>
2016-04-03 18:46:05 +0200

    Updated IdPlugin

--- src/TelegramBot/plugins/idPlugin.php
+++ src/TelegramBot/plugins/idPlugin.php
@@ -1,20 +1,21 @@
 <?php
 require_once(__DIR__ . '/../PluginManager.php');
 
-return 'IdPlugin';
+
 class IdPlugin extends TB_Plugin {
   public function IdPlugin($api) {
     parent::__construct($api);
   }
 
-  public function onMessageReceived($message) {
-    if ($message->hasText()) {
-      if ($message->getText() === "/id" || $message->getText() === "/start id") {
-        $message->sendReply($message->getFrom()->getId());
-      }
-    }
-    if ($message->isForwarded()) {
-      $message->sendReply("Forwarded from: ".$message->getForwardFrom()->getId());
-    }
+  /**
+   * %condition text matches ^\/(?:id|start id)(?:@{#USERNME})?$
+   */
+  public function onTextMessageReceived($message) {
+    $message->sendReply($message->getFrom()->getId());
   }
 }
+return array(
+  'class' => 'IdPlugin',
+  'name' => 'Id',
+  'id' => 'IdPlugin'
+);
