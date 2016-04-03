6c8f9edfeee1bcd9e86012909f978c2f080e36dc
DevPGSV <DevPGSV@gmail.com>
2016-04-03 18:45:56 +0200

    Updated helloWorldPlugin

---
+++ src/TelegramBot/plugins/helloWorldPlugin.php
@@ -0,0 +1,22 @@
+<?php
+require_once(__DIR__ . '/../PluginManager.php');
+
+
+class HelloWorldPlugin extends TB_Plugin {
+  public function HelloWorldPlugin($api) {
+    parent::__construct($api);
+  }
+
+  /**
+   * %condition text matches ^hi$
+   */
+  public function onMessageReceived($message, &$eventData, $method) {
+    $message->sendReply("Hello world from HelloWorldPlugin!");
+  }
+
+}
+return array(
+  'class' => 'HelloWorldPlugin',
+  'name' => 'Hello World',
+  'id' => 'HelloWorldPlugin'
+);
