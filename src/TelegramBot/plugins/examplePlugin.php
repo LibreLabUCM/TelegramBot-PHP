ed694c513a15843384ae51090c1e0eec4e4f657f
DevPGSV <DevPGSV@gmail.com>
2016-04-10 18:47:21 +0200

    Improved example plugin

--- src/TelegramBot/plugins/examplePlugin.php
+++ src/TelegramBot/plugins/examplePlugin.php
@@ -12,15 +12,17 @@ class ExamplePlugin extends TB_Plugin {
    *    anything with priority from 0 to 4 will be called before this function,
    *    with priority 6 or greater will be called after this function.
    * %priority 5
-   * 		same, but with 10 // 2 priorities => this function will be called 2 times!
-   * %priority 10
+   * 		same, but with 10 // 2 priorities => this function will be called 2 times! The "." deactivates this, so tha this is called only once.
+   * %.priority 10
    *    if it is a message
    * %condition isMessage
    *    if the message is a text message
    * %condition hasText
    *    if the text is "/example" or contains the bot @username
-   * %condition text matches (@{#USERNME})|(^\/example$)
+   * %condition text matches ^(@{#USERNME})|(\/example)$
    *    this function will be called if conditions are met, even if the event was cancelled
+   * %condition date any
+   * 		'any' => alaways | 'isNew' => only if the message was just sent | 'isOld' => only if the message was sent some time ago, but the bot just received it (perhaps the hook was not set correctly, or the bot didn't return the 020 header to telegram requests)
    * %callEvenIfCancelled true
    */
   public function onMessageReceived($message, &$eventData, $method) {
@@ -28,7 +30,7 @@ class ExamplePlugin extends TB_Plugin {
     $t = $message->getText(); // Gets the message text. We are sure the message has text, it is specified as a %condition
     $c = $eventData['cancelled'] ? 'yes' : 'no'; // Gets if the event was cancelled.
     $timesProcessed = $eventData['timesProcessed']; // Gets the number of times this event was processed
-    $message->sendReply("Priority: $p\n Text: $t\n Cancelled: $c\n Times processed: $timesProcessed\n"); // Replies to the message with specified text
+    $message->sendReply(">*Priority*: `$p`\n>*Text*: ```$t```\n>*Cancelled*: `$c`\n>*Times processed*: `$timesProcessed`\n>*Metadata*: ```".print_r($method['metadata'], true)."```\n>*Method*: ```".print_r($method['method'], true)."```", null, null, 'Markdown'); // Replies to the message with specified text
     $eventData['cancelled'] = true; // Cancels the event. No other plugins will process the event (except those with %callEvenIfCancelled true)
   }
 
