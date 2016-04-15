9ad46f6868216cde0068a7da95ff82908d95d022
DevPGSV <DevPGSV@gmail.com>
2016-04-15 16:29:23 +0200

    Added test commands

--- src/TelegramBot/plugins/testPlugin.php
+++ src/TelegramBot/plugins/testPlugin.php
@@ -13,8 +13,8 @@ class TestPlugin extends TB_Plugin {
   public function onTextMessageReceived($message, &$eventData, $method) {
     $t = str_replace('@'.$this->bot->getBotUsername(), '', trim($message->getText()));
     if ($t === "/test") {
-      $k = new TA_ReplyKeyboardMarkup([['/test'],['/test_reply'],['/test_typing'],['/test_forceReplay'],['/test_keyboard'],['/test_hideKeyboard'],['/test_profilephotos']], null, true);
-      $this->api->sendMessage($message->getFrom(), "/test\n/test_reply\n/test_typing\n/test_forceReplay\n/test_keyboard\n/test_hideKeyboard\n/test_profilephotos", null, null, $k);
+      $k = new TA_ReplyKeyboardMarkup([['/test'],['/test_reply'],['/test_typing'],['/test_forceReplay'],['/test_keyboard'],['/test_hideKeyboard'],['/test_profilephotos'],['/test_entities'],['/test_info'],['/test_edit']], null, true);
+      $this->api->sendMessage($message->getFrom(), "/test\n/test_reply\n/test_typing\n/test_forceReplay\n/test_keyboard\n/test_hideKeyboard\n/test_profilephotos\n/test_entities\n/test_info\n/test_edit", null, null, $k);
     } else if ($t === "/test_keyboard") {
       $k = new TA_ReplyKeyboardMarkup([[' - - - ']]); // 0
       $k->addRow()->addOption("/test_hideKeyboard") // 1
