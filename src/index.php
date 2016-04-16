00dba4a3ff4ce7d09382401d4c8f8fdba0ca232e
DevPGSV <DevPGSV@gmail.com>
2016-04-16 14:38:10 +0200

    Display plugin info and changelog in index

--- src/index.php
+++ src/index.php
@@ -16,3 +16,32 @@ if (isset($_GET['setwebhook'])) {
   }
   exit();
 }
+
+
+echo "<h1>Plugins</h1>\n";
+$plugins =  $bot->getPluginManager()->getPluginList();
+
+echo "<ul>\n";
+foreach ($plugins as $pluginId => $plugin) {
+  echo "<li><h3>".$plugin['name']." ($pluginId)</h3>\n";
+  $changelog = $plugin['plugin']->getChangeLog();
+  echo '<table border="1">';
+  foreach ($changelog as $timestamp => $update) {
+    $version = $update['version'][0].'.'.$update['version'][1].'.'.$update['version'][2];
+    if (!empty($update['version'][3])) $version .= '-'.$update['version'][3];
+    echo '<tr>';
+    echo '<td>'.date('d/m/y H:i', $timestamp).'</td>';
+    echo '<td>'.$version.'</td>';
+    echo '<td>';
+    echo "<ul>\n";
+    foreach ($update['changes'] as $change) {
+      echo '<li>'.$change.'</li>';
+    }
+    echo "</ul>\n";
+    echo '</td>';
+    echo '</tr>';
+  }
+  echo '<table>';
+  echo "</li>\n";
+}
+echo "</ul>\n";
